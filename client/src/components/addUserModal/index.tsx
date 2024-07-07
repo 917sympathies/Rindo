c0e8015610f7f97bafa2ea562c944fbeb2e107c5
917sympathies <maraturazbakhtin@yandex.ru>
2024-07-07 22:00:19 +0400

    addUserModal refactoring

--- client/src/components/addUserModal/index.tsx
+++ client/src/components/addUserModal/index.tsx
@@ -1,8 +1,7 @@
 "use client";
-import styles from "./styles.module.css";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
-import { User2, X, UserRoundPlus } from "lucide-react";
+import { User2, X } from "lucide-react";
 import { useState } from "react";
 import { useParams } from "next/navigation";
 
@@ -28,35 +27,11 @@ export default function AddUserModal({ onClose }: ModalProps) {
   };
 
   return (
-    <div
-      className="bg-white dark:bg-[#111] dark:border-black/20 text-black dark:text-white"
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        width: "100%",
-        borderRadius: "8px",
-        padding: "10px",
-      }}
-    >
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          alignItems: "center",
-          justifyContent: "space-between",
-          margin: "0.2rem 0.2rem 1rem 0.2rem",
-        }}
-      >
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-          }}
-        >
+    <div className="flex flex-col justify-center w-full rounded-[8px] p-[10px] bg-white dark:bg-[#111] dark:border-black/20 text-black dark:text-white">
+      <div className="flex flex-row items-center justify-between m-[0.2rem] mb-[1rem]">
+        <div className="flex flex-row items-center">
           <User2 size={24} />
-          <div style={{ marginLeft: "0.2rem" }}>Добавить пользователя</div>
+          <div className="ml-[0.2rem]">Добавить пользователя</div>
         </div>
         <X
           size={24}
@@ -64,15 +39,7 @@ export default function AddUserModal({ onClose }: ModalProps) {
           className="p-1 rounded-full hover:bg-black/10 ease-in-out duration-300"
         ></X>
       </div>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          alignItems: "center",
-          justifyContent: "space-between",
-          gap: 8,
-        }}
-      >
+      <div className="flex flex-row items-center justify-between gap-2">
         <Input
           className="focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-slate-950 focus-visible:ring-offset-0 dark:border-black/40"
           placeholder="Имя пользователя"
@@ -80,13 +47,11 @@ export default function AddUserModal({ onClose }: ModalProps) {
           onChange={(e) => setUsername(e.target.value)}
         />
         <Button
-          className="text-white bg-blue-500 hover:bg-blue-800"
-          style={{ marginRight: "0.4rem" }}
+          className="text-white bg-blue-500 hover:bg-blue-800 mr-[0.4rem]"
           onClick={() => handleInviteUser()}
         >
           Отправить
         </Button>
-        {/* <UserRoundPlus size={36}/> */}
       </div>
     </div>
   );
